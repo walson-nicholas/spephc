1791885ce17450f043155c79ba4730eefbbb1fb7
NICHOLAS\asimi <asimieanicholas@gmail.com>
2023-02-12 14:38:58 +0100

    added header hero section

---
+++ src/assets/components/CountdownTimer.js
@@ -0,0 +1,41 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React, { useState, useEffect } from "react"
+import '../css/header.css'
+
+export default function CountdownTimer() {
+    // Set the target date and time
+    const targetDate = new Date("Apr 14, 2023 09:00:00");
+    
+    // State variables to store the remaining time
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            // Get the current date and time
+            const currentDate = new Date();
+            
+            // Calculate the difference in milliseconds
+            const diff = targetDate - currentDate;
+            
+            // Calculate the number of days, hours, minutes, and seconds remaining
+            setDays(Math.floor(diff / (1000 * 60 * 60 * 24)));
+            setHours(Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+            setMinutes(Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)));
+            setSeconds(Math.floor((diff % (1000 * 60)) / 1000));
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <div className='CountdownTimer'>
+            <span style={{ color: 'red' }}>{days} days</span>
+            <span style={{ color: 'purple' }}>{hours} hours</span> 
+            <span style={{ color: 'blue' }}>{minutes} minutes</span>
+            <span style={{ color: 'orange' }}>{seconds} seconds</span>
+        </div>
+    );
+}
--- src/assets/components/Header.js
+++ src/assets/components/Header.js
@@ -1,17 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import CountdownTimer from "./CountdownTimer"
 import React from "react"
 import '../css/header.css'
 
 export default function Header() {
     return (
         <header>
-            <h3>Society of Petroleum Engineers, Port Harcourt (Section 103)</h3>
+            <h3>Society of Petroleum Engineers, Port Harcourt (Section 103)</h3>
+
             <h1>2023 YOUNG PROFESSIONAL SYMPOSIUM</h1>
+
             <div>
-                <div>
-                    <i class="fa-solid fa-location-dot"></i>
+                <div class="header_icons">
+                    <i class="gg-pin"></i>
+                    <span>Casoni International Hotels and Suites</span>
                 </div>
+
+                <div class="header_icons">
+                    <i class="gg-calendar-dates"></i>
+                    <span>Friday, 14th April - Saturday, 15th April 2023</span>
+                </div>
+            </div>
+        
+            <CountdownTimer />
+
+            <div class="header-buttons">
+                <button type="button" class="btn btn-danger btn-sm" href="#" role="button">REGISTER</button>
+                <button type="button" class="btn btn-info btn-sm" href="#" role="button">DOWNLOAD BROCHURE</button>
+                <button type="button" class="btn btn-warning btn-sm" href="#" role="button">CREATE PERSONALISED FLYER</button>
             </div>
         </header>
     )
