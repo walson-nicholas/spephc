a1619f675c176ae24afb226b262b365b4acf03af
NICHOLAS\asimi <asimieanicholas@gmail.com>
2023-03-04 09:47:01 +0100

    reponsive panelist

--- src/assets/components/Panelist.js
+++ src/assets/components/Panelist.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '../css/panelist.css'
-import effiom from "../img/effiom.jpg"
+import uche from "../img/uche.jpg"
 import ifeoma from "../img/ifeoma.png"
 import ayuk from "../img/ayuk.jpg"
 
@@ -9,7 +9,7 @@ export default function Panelist() {
     return (
         <section class="panelist" id='panelist-section'>
             <h2>PANEL SESSION</h2>
-            <p>Meet our panelist</p>
+            <p>Meet our Panelists</p>
             <div class="accordion" id="accordionExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="headingOne">
@@ -20,24 +20,29 @@ export default function Panelist() {
                             data-bs-target="#collapseOne" 
                             aria-expanded="true" 
                             aria-controls="collapseOne">
-                            1.	Oghogho Effiom (Special Guest of Honour)
+                            1.	Johnbosco Uche (FNAPE)
                         </button>
                     </h2>
                     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div class="accordion-body panel-content">
-                            <img src={effiom} />
+                            <img src={uche} />
                             <text>
-                                <strong>Regional Director, Africa for Society of Petroleum Engineers:</strong> <br />
-                                Oghogho Effiom is the Regional Director, Africa for Society of Petroleum Engineers, sitting on the SPE International Board. 
-                                Oghogho has over 22 years of technical experience in the Upstream oil and gas businesses with assignments in Nigeria and the 
-                                United States covering offshore drilling operations, reserves estimation, field development and execution.
-                                She is also currently the domestic gas business opportunity manager for SPDC, a role that integrates technical and commercial 
-                                gas projects maturation to support Nigeria’s gas utilization aspiration. She plays a key governance role in risk management, 
-                                stakeholder engagement, and budget alignment driving end to end integration of these elements to ensure gas is delivered to domestic markets. 
-                                Oghogho is a multi award winner recognized as as one of the 100 most influential women in the oil and has industry in Africa, 
-                                the international professional Grit award, the SPE Regional service award as well as the woman in offshore and maritime award winner. 
-                                She has also been recognized in Shell with an Upstream impact award winner as well as the Development Function Platinum award winner.
-
+                                <strong>Energy Transition Business Opportunity Manager for Shell Companies, Nigeria:</strong> <br />
+                                Johnbosco Uche (JB) is the Energy Transition Business Opportunity Manager for Shell Companies in Nigeria. 
+                                He also doubles as Front-End Development Manager and Project Leader in Shell Petroleum Development Company 
+                                of Nigeria (SPDC). 
+                                JB is a Geological Science graduate of Nnamdi Azikiwe University Awka. He obtained his Master’s in Business 
+                                Administration (MBA) from Edinburgh Business School, Heriot-Watt University, Scotland. 
+                                JB has acquired over 22 years’ experience in the energy industry working for Shell, CONOIL Producing, and 
+                                Halliburton across technical and business roles. Since joining Shell in 2005, he has worked as Exploration 
+                                Geoscientist & Team Lead in SPDC Nigeria, Shell Netherlands, Shell United-Kingdom, and most recently as Shell 
+                                Nigeria Exploration and Production Company (SNEPCo) Exploration Manager between 2018 and 2021.
+                                Johnbosco is a former Vice president and Fellow of Nigerian Association of Petroleum Explorationists (NAPE). 
+                                He is registered with Council of Mining Engineers and Geoscientists (COMEG) and also a member of NMGS, AAPG, 
+                                SEG, EAGE, and PESGB.
+                                Due to his strong passion for organic talent development of young Nigerian Geoscientists, he initiated the 
+                                novel annual NAPE Mentoring Circles for emerging leaders in the industry.
+                                Johnbosco is married with children.
                                 <br />
                                 <a href='https://www.linkedin.com/in/oghogho-effiom/?originalSubdomain=ng'>
                                     <button class="btn btn-primary pn-btn">LinkedIn</button>
@@ -62,7 +67,7 @@ export default function Panelist() {
                     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div class="accordion-body panel-content revImg">
                             <text>
-                                <strong>Co-Founder/ CEO, of Clean Technology Hub Nigeria</strong> <br />
+                                <strong>Co-Founder/ CEO, Clean Technology Hub Nigeria</strong> <br />
                                 Ifeoma Malo is an organizational management expert with over 23 years experience, building and directing organizational 
                                 policy and strategy in energy and large-scale utility markets, public health, biotechnology, leadership, change management 
                                 and infrastructure finance. She is the Co-Founder/ CEO, of Clean Technology Hub Nigeria.
@@ -129,7 +134,7 @@ export default function Panelist() {
                             data-bs-target="#collapseFour" 
                             aria-expanded="false" 
                             aria-controls="collapseFour">
-                            4.	Yetunde Fadeyi 
+                            4.	Speaker name
                         </button>
                     </h2>
                     <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
@@ -143,7 +148,7 @@ export default function Panelist() {
                                 <br />
                                 <button class="btn btn-primary pn-btn">View more</button>
                             </text>
-                            <img src={effiom} />
+                            <img src={uche} />
                         </div>
                     </div>
                 </div>
@@ -157,12 +162,12 @@ export default function Panelist() {
                             data-bs-target="#collapseFive" 
                             aria-expanded="false" 
                             aria-controls="collapseFive">
-                            5.	Spaker name
+                            5.	Speaker name
                         </button>
                     </h2>
                     <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                         <div class="accordion-body panel-content">
-                            <img src={effiom} />
+                            <img src={uche} />
                             <text>
                                 <strong>Spaker's portfolio</strong> <br />
                                 Experienced Well Engineer with a demonstrated history of working in the oil & energy industry. 
